a480f208762c89bc078d9b4b86c70517160f4dd5
alicevoid <amelia.pvc@gmail.com>
2026-05-12 17:13:48 -0700

    doing my best to get started

--- README.md
+++ README.md
@@ -3,6 +3,4 @@
 A nixos file-transfer tool designed for the CLI that implements HTTP2 to-spec in python.
 
 # etc...
-lmd: 
-- alicevoid
-- 05/05/2026
+- alicevoid: 05/05/2026
---
+++ server.py
@@ -0,0 +1,15 @@
+"""
+Server Design
+
+When someone initiates a file transfer:
+- A server is spun up
+- The server listens for incoming connections
+- When a client connects, the server serves the specified file/dir to the client
+- Thats about it lol
+
+alicevoid: 05/12/2026
+"""
+
+# Server Initialization 
+
+# Command Line Arguments
